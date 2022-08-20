40f4ebb7715d8d6c9009c5774917f5235fd3495d
dtinth on MBP M1 <dtinth@spacet.me>
2022-08-20 20:16:59 +0700

    Add code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export {}
+/**
+ * Creates a type helper that takes an object of type T and returns it.
+ * @public
+ */
+export function createTypeHelper<T>() {
+  return (value: T) => value
+}
+
+/**
+ * Creates a type helper that infers the type of the value constraining it to the given type.
+ * @public
+ */
+export function createSubtypeHelper<T>() {
+  return <X extends T>(value: X) => value
+}
